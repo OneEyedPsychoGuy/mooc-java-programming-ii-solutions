b8d0756ac6188d922899edf7b87dec3c4903c9c1
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-08-12 22:31:05 -0700

    Add part08-Part08_14.DictionaryOfManyTranslations

---
+++ part08/part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+public class DictionaryOfManyTranslations {
+    private Map<String, List<String>> translations;
+
+    public DictionaryOfManyTranslations() {
+        this.translations = new HashMap<>();
+    }
+
+    public void add(String word, String translation) {
+        this.translations.putIfAbsent(word, new ArrayList<>());
+        this.translations.get(word).add(translation);
+    }
+
+    public ArrayList<String> translate(String word) {
+        return (ArrayList<String>) this.translations.getOrDefault(word, new ArrayList<>());
+    }
+
+    public void remove(String word) {
+        this.translations.remove(word);
+    }
+}
