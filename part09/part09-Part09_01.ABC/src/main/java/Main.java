18d37eb6f31bc4e846d75972d637389099008745
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-08-14 22:22:04 -0700

    Add part09-Part09_01.ABC Part 2: Class inheritance

--- part09/part09-Part09_01.ABC/src/main/java/Main.java
+++ part09/part09-Part09_01.ABC/src/main/java/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        A a = new A();
-        B b = new B();
         C c = new C();
 
-        a.a();
-        b.b();
+        c.a();
+        c.b();
         c.c();
     }
 }
