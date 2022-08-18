0ef07d1058d8fdc073ceedc3675aee9fb1637237
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-08-17 21:32:47 -0700

    Add part09-Part09_03.Warehousing Part 3: Change History, step 1

---
+++ part09/part09-Part09_03.Warehousing/src/main/java/ChangeHistory.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ChangeHistory {
+    private List<Double> history;
+
+    public ChangeHistory() {
+        this.history = new ArrayList<>();
+    }
+
+    public void add(double status) {
+        this.history.add(status);
+    }
+
+    public void clear() {
+        this.history.clear();
+    }
+
+    @Override
+    public String toString() {
+        return this.history.toString();
+    }
+}
