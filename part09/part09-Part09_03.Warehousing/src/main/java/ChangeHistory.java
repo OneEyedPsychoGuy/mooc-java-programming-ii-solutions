a655abf5e6d67d194182382465328d3f8432efe0
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-08-17 21:43:56 -0700

    Add part09-Part09_03.Warehousing Part 4: Change History, step 2

--- part09/part09-Part09_03.Warehousing/src/main/java/ChangeHistory.java
+++ part09/part09-Part09_03.Warehousing/src/main/java/ChangeHistory.java
@@ -1,5 +1,7 @@
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ChangeHistory {
     private List<Double> history;
@@ -14,6 +16,38 @@ public class ChangeHistory {
 
     public void clear() {
         this.history.clear();
+    }
+
+    public double maxValue() {
+        double max = Double.MIN_VALUE;
+
+        for(double value : this.history) {
+            if(value > max) max = value;
+        }
+
+        if(max == Double.MIN_VALUE) return 0;
+        return max;
+    }
+
+    public double minValue() {
+        double min = Double.MAX_VALUE;
+
+        for(double value : this.history) {
+            if(value < min) min = value;
+        }
+
+        if(min == Double.MAX_VALUE) return 0;
+        return min;
+    }
+
+    public double average() {
+        double sum = 0;
+
+        for(double value : this.history) {
+            sum += value;
+        }
+
+        return sum / this.history.size();
     }
 
     @Override
