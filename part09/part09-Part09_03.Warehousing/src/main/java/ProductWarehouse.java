ee10e07dd3622cfb96821eff20751315a2834832
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-08-17 21:27:00 -0700

    Add part09-Part09_03.Warehousing Part 2: Product warehouse, step 2

--- part09/part09-Part09_03.Warehousing/src/main/java/ProductWarehouse.java
+++ part09/part09-Part09_03.Warehousing/src/main/java/ProductWarehouse.java
@@ -9,4 +9,13 @@ public class ProductWarehouse extends Warehouse {
     public String getName() {
         return this.productName;
     }
+
+    public void setName(String newName) {
+        this.productName = newName;
+    }
+
+    @Override
+    public String toString() {
+        return this.productName + ": " + super.toString();
+    }
 }
