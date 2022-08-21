1c01c69ce37aa170c0209956338c98787e1ae331
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-08-20 23:13:06 -0700

    Add part09-Part09_06.InterfaceInABox Part 3: Box weight

--- part09/part09-Part09_06.InterfaceInABox/src/main/java/Box.java
+++ part09/part09-Part09_06.InterfaceInABox/src/main/java/Box.java
@@ -4,29 +4,30 @@ import java.util.ArrayList;
 public class Box implements Packable {
     private List<Packable> items;
     private double capacity;
-    private double weight;
 
     public Box(double capacity) {
         this.items = new ArrayList<>();
         this.capacity = capacity;
-        this.weight = 0;
     }
 
     public void add(Packable item) {
-        double totalWeight = this.weight + item.weight();
+        double totalWeight = this.weight() + item.weight();
         if(totalWeight <= capacity) {
             this.items.add(item);
-            this.weight = totalWeight;
         }
     }
 
     @Override
     public double weight() {
-        return this.weight;
+        double totalWeight = 0;
+        for(Packable item : this.items) {
+            totalWeight += item.weight();
+        }
+        return totalWeight;
     }
 
     @Override
     public String toString() {
-        return "Box: " + this.items.size() + " items, total weight " + this.weight + " kg";
+        return "Box: " + this.items.size() + " items, total weight " + this.weight() + " kg";
     }
 }
