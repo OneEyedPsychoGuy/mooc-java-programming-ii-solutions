8b1d435040b97200bf0165496a755540d4fa7586
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-08-24 22:03:40 -0700

    Add part09-Part09_10.OnlineShop Part 2: Products stock balance

--- part09/part09-Part09_10.OnlineShop/src/main/java/Warehouse.java
+++ part09/part09-Part09_10.OnlineShop/src/main/java/Warehouse.java
@@ -3,16 +3,33 @@ import java.util.HashMap;
 
 public class Warehouse {
     private Map<String, Integer> productPrice;
+    private Map<String, Integer> productStock;
 
     public Warehouse() {
         this.productPrice = new HashMap<>();
+        this.productStock = new HashMap<>();
     }
 
     public void addProduct(String product, int price, int stock) {
         this.productPrice.put(product, price);
+        this.productStock.put(product, stock);
     }
 
     public int price(String product) {
         return this.productPrice.getOrDefault(product, -99);
+    }
+
+    public int stock(String product) {
+        return this.productStock.getOrDefault(product, 0);
+    }
+
+    public boolean take(String product) {
+        int currentStock = this.productStock.getOrDefault(product, 0);
+        if(currentStock <= 0) {
+            return false;
+        }
+
+        this.productStock.put(product, currentStock - 1);
+        return true;
     }
 }
