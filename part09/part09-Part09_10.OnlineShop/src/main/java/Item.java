45d4e121c83c5fce510408bf8740d74088194cba
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-08-25 22:52:52 -0700

    Add part09-Part09_10.OnlineShop Part 4: Item

---
+++ part09/part09-Part09_10.OnlineShop/src/main/java/Item.java
@@ -0,0 +1,24 @@
+public class Item {
+    private String name;
+    private int quantity;
+    private int price;
+
+    public Item(String name, int quantity, int price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int price() {
+        return this.quantity * this.price;
+    }
+
+    public void increaseQuantity() {
+        this.quantity++;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ": " + this.quantity;
+    }
+}
