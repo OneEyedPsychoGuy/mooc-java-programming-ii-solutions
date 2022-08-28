f6d9b165ed684c62f8678f9aed54284eda7694ea
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-08-27 22:53:53 -0700

    Add part09-Part09_12.Animals Part 2: Dog

---
+++ part09/part09-Part09_12.Animals/src/main/java/Dog.java
@@ -0,0 +1,13 @@
+public class Dog extends Animal {
+    public Dog() {
+        super("Dog");
+    }
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    public void bark() {
+        System.out.println(this.getName() + " barks");
+    }
+}
