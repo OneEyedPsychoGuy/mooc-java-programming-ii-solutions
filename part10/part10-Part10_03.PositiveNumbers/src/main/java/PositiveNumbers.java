79ee42f4e70b70a48223d240136f27358cf08730
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-08-28 23:22:52 -0700

    Refactor part10-Part10_03.PositiveNumbers

--- part10/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,11 +1,12 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PositiveNumbers {
     public static void main(String[] args) {
-
+    
     }
 
     public static List<Integer> positive(List<Integer> numbers) {
-        return numbers.stream().filter(num -> num > 0).toList();
+        return numbers.stream().filter(num -> num > 0).collect(Collectors.toList());
     }
 }
