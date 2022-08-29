835c9615d89000c643f84dbea55cdf44ef4300d2
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-08-28 23:13:50 -0700

    Add part10-Part10_03.PositiveNumbers

---
+++ part10/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class PositiveNumbers {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> positive(List<Integer> numbers) {
+        return numbers.stream().filter(num -> num > 0).toList();
+    }
+}
