7c552f78b1aeadb9b2f725adaea4bf40aa2f4d44
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-09-05 22:32:43 -0700

    Add part10-Part10_15.RegularExpressions Part 2: Vowel check

--- part10/part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10/part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -2,4 +2,8 @@ public class Checker {
     public boolean isDayOfWeek(String string) {
         return string.matches("mon|tue|wed|thu|fri|sat|sun");
     }
+
+    public boolean allVowels(String string) {
+        return string.matches("[aeiou]*");
+    }
 }
