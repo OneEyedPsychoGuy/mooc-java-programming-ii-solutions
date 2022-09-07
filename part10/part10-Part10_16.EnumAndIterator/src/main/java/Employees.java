5076d00409f56587451853456c5f629e51ff83c6
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-09-06 23:10:31 -0700

    Add part10-Part10_16.EnumAndIterator Part 4: Firing an employee

--- part10/part10-Part10_16.EnumAndIterator/src/main/java/Employees.java
+++ part10/part10-Part10_16.EnumAndIterator/src/main/java/Employees.java
@@ -1,30 +1,45 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Employees {
-    private List<Person> persons;
+    private List<Person> employees;
 
     public Employees() {
-        this.persons = new ArrayList<>();
+        this.employees = new ArrayList<>();
     }
 
     public void add(Person person) {
-        this.persons.add(person);
+        this.employees.add(person);
     }
 
     public void add(List<Person> people) {
-        this.persons.addAll(people);
+        this.employees.addAll(people);
     }
 
     public void print() {
-        this.persons.forEach(System.out::println);
+        Iterator<Person> employeeIterator = this.employees.iterator();
+        while(employeeIterator.hasNext()) {
+            System.out.println(employeeIterator.next());
+        }
     }
 
     public void print(Education education) {
-        this.persons.forEach(person -> {
-            if(person.getEducation() == education) {
-                System.out.println(person);
+        Iterator<Person> employeeIterator = this.employees.iterator();
+        while(employeeIterator.hasNext()) {
+            Person employee = employeeIterator.next();
+            if(employee.getEducation() == education) {
+                System.out.println(employee);
             }
-        });
+        }
+    }
+
+    public void fire(Education education) {
+        Iterator<Person> employeeIterator = this.employees.iterator();
+        while(employeeIterator.hasNext()) {
+            if(employeeIterator.next().getEducation() == education) {
+                employeeIterator.remove();
+            }
+        }
     }
 }
