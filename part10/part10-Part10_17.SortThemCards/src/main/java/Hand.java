50ce99cd74c962425ec0c2faf3b1d34e674871ef
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-09-07 22:16:48 -0700

    Add part10-Part10_17.SortThemCards Part 4: Comparing hands

--- part10/part10-Part10_17.SortThemCards/src/main/java/Hand.java
+++ part10/part10-Part10_17.SortThemCards/src/main/java/Hand.java
@@ -2,7 +2,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Hand {
+public class Hand implements Comparable<Hand> {
     private List<Card> cards;
 
     public Hand() {
@@ -19,5 +19,12 @@ public class Hand {
 
     public void sort() {
         Collections.sort(this.cards);
+    }
+
+    @Override
+    public int compareTo(Hand hand) {
+        int thisHandValue = this.cards.stream().mapToInt(card -> card.getValue()).reduce(0, (sum, value) -> sum + value);
+        int comparedHandValue = hand.cards.stream().mapToInt(card -> card.getValue()).reduce(0, (sum, value) -> sum + value);
+        return thisHandValue - comparedHandValue;
     }
 }
