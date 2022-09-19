cdd2062cbd59edece57c23bd7c7fc8edcd600fc2
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-09-18 22:17:50 -0700

    Add part11-Part11_11.ValidatingParameters Part 1: Validating a person

---
+++ part11/part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -0,0 +1,23 @@
+package validating;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        if(name.isEmpty() || name == null || name.length() > 40 || age < 0 || age > 120) {
+            throw new IllegalArgumentException("Invalid name or age!");
+        }
+
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
