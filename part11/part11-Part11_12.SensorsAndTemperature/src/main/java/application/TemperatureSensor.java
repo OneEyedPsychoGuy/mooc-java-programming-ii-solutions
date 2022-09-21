5f3747881291be49fd625c93870b5697680f87cd
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-09-20 23:02:58 -0700

    Add part11-Part11_12.SensorsAndTemperature Part 2: TemperatureSensor

---
+++ part11/part11-Part11_12.SensorsAndTemperature/src/main/java/application/TemperatureSensor.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Random;
+
+public class TemperatureSensor implements Sensor {
+    private boolean running;
+
+    public TemperatureSensor() {
+        this.running = false;
+    }
+
+    @Override
+    public boolean isOn() {
+        return this.running;
+    }
+
+    @Override
+    public void setOn() {
+        this.running = true;
+    }
+
+    @Override
+    public void setOff() {
+        this.running = false;
+    }
+
+    @Override
+    public int read() {
+        if(!this.isOn()) {
+            throw new IllegalStateException("Cannot perform read() method when sensor is off!");
+        }
+
+        return new Random().nextInt(61) - 30;
+    }
+}
