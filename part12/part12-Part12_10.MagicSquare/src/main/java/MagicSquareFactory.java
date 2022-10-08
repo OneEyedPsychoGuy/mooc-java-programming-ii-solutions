436eb25f2aaa2cdf5fbf73259399b88fa7d1ae29
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-10-08 00:14:37 -0700

    Add part12-Part12_10.MagicSquare Part 4: Conjuring a magic square

--- part12/part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
+++ part12/part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
@@ -1,8 +1,29 @@
 public class MagicSquareFactory {
     public MagicSquare createMagicSquare(int size) {
         MagicSquare square = new MagicSquare(size);
+        int filled = 0;
+        int row = 0;
+        int col = size / 2;
 
-        // implement the creation of a magic square with the Siamese method algorithm here
+        while(filled < size*size) {
+            if(row < 0) {
+                row = size - 1;
+            }
+            if(col >= size) {
+                col = 0;
+            }
+
+            square.placeValue(col, row, ++filled);
+
+            if(filled % size == 0) {
+                row++;
+                continue;
+            } 
+
+            row--;
+            col++;
+        }
+
         return square;
     }
 }
