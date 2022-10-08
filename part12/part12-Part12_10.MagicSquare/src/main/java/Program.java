e793885f6f0c21250eab67cb44d9f3db6234552c
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-10-07 22:46:19 -0700

    Add part12-Part12_10.MagicSquare Part 1: Sum of rows

---
+++ part12/part12-Part12_10.MagicSquare/src/main/java/Program.java
@@ -0,0 +1,6 @@
+public class Program {
+    public static void main(String[] args) {
+        MagicSquareFactory msFactory = new MagicSquareFactory();
+        System.out.println(msFactory.createMagicSquare(5));
+    }
+}
