de8cad6fa804aeea9d4dac42bf55eed8c0fe0b01
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-10-15 23:42:51 -0700

    Add part13-Part13_02.ButtonAndLabel

---
+++ part13/part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
@@ -0,0 +1,27 @@
+package buttonandlabel;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class ButtonAndLabelApplication extends Application {
+    public static void main(String[] args) {
+        launch(ButtonAndLabelApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        Label label = new Label("Label");
+        Button button = new Button("Button");
+
+        FlowPane pane = new FlowPane();
+        pane.getChildren().add(label);
+        pane.getChildren().add(button);
+
+        window.setScene(new Scene(pane));
+        window.show();
+    }
+}
