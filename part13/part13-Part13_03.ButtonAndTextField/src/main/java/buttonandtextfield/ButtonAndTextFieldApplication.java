1679c3d1ca668afae840d2991540b740017ffed1
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-10-16 00:04:29 -0700

    Add part13-Part13_03.ButtonAndTextField

---
+++ part13/part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
@@ -0,0 +1,27 @@
+package buttonandtextfield;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class ButtonAndTextFieldApplication extends Application {
+    public static void main(String[] args) {
+        launch(ButtonAndTextFieldApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        Button button = new Button("Button");
+        TextField text = new TextField();
+
+        FlowPane pane = new FlowPane();
+        pane.getChildren().add(button);
+        pane.getChildren().add(text);
+
+        window.setScene(new Scene(pane));
+        window.show();
+    }
+}
