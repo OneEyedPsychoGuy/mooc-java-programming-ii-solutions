d6fa5a07ae7179eabf6712c9f2419b630dce2cb9
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-11-10 23:24:31 -0800

    Add part14-Part14_07.Collage Part 2: Grid

--- part14/part14-Part14_07.Collage/src/main/java/collage/CollageApplication.java
+++ part14/part14-Part14_07.Collage/src/main/java/collage/CollageApplication.java
@@ -4,11 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.PixelReader;
-import javafx.scene.image.PixelWriter;
-import javafx.scene.image.WritableImage;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class CollageApplication extends Application {
@@ -33,14 +29,56 @@ public class CollageApplication extends Application {
         return image;
     }
 
+    private ImageView getTopRightImageView(Image source) {
+        double width = source.getWidth();
+        double height = source.getHeight();
+
+        ImageView image = new ImageView(source);
+        image.setScaleX(0.5);
+        image.setScaleY(0.5);
+        image.setTranslateX(width * 0.25);
+        image.setTranslateY(height * -0.25);
+
+        return image;
+    }
+
+    private ImageView getBottomLeftImageView(Image source) {
+        double width = source.getWidth();
+        double height = source.getHeight();
+
+        ImageView image = new ImageView(source);
+        image.setScaleX(0.5);
+        image.setScaleY(0.5);
+        image.setTranslateX(width * -0.25);
+        image.setTranslateY(height * 0.25);
+
+        return image;
+    }
+
+    private ImageView getBottomRightImageView(Image source) {
+        double width = source.getWidth();
+        double height = source.getHeight();
+
+        ImageView image = new ImageView(source);
+        image.setScaleX(0.5);
+        image.setScaleY(0.5);
+        image.setTranslateX(width * 0.25);
+        image.setTranslateY(height * 0.25);
+
+        return image;
+    }
+
     @Override
     public void start(Stage stage) {
         Image source = new Image("file:monalisa.png");
 
         ImageView image = getFullImageView(source);
         ImageView topLeftImage = getTopLeftImageView(source);
+        ImageView topRightImage = getTopRightImageView(source);
+        ImageView bottomImageLeftView = getBottomLeftImageView(source);
+        ImageView bottomImageRightView = getBottomRightImageView(source);
 
-        stage.setScene(new Scene(new Pane(image, topLeftImage)));
+        stage.setScene(new Scene(new Pane(image, topLeftImage, topRightImage, bottomImageLeftView, bottomImageRightView)));
         stage.show();
     }
 }
