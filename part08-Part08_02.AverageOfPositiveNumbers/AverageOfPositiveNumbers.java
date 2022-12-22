ea38d2b92865a3dff7088d10579efa073bd128ca
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-21 19:58:06 -0800

    Finalize part08-Part08_01.Cubes

--- part08-Part08_02.AverageOfPositiveNumbers/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/AverageOfPositiveNumbers.java
@@ -2,26 +2,25 @@ import java.util.Scanner;
 
 public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        int count = 0, sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        int num = -1;
+        int count = 0;
+        int sum = 0;
 
-        while(true) {
-            int num = Integer.valueOf(keyboard.nextLine());
-            
-            if(num == 0) {
-                break;
-            } else if(num > 0) {
+        while(num != 0) {
+            System.out.println("Give a number:");
+            num = Integer.valueOf(scanner.nextLine());
+            if(num > 0) {
                 count++;
                 sum += num;
             }
         }
+        scanner.close();
 
-        if(count != 0) {
+        if(count > 0) {
             System.out.println(1.0 * sum / count);
         } else {
             System.out.println("Cannot calculate the average");
         }
-
-        keyboard.close();
     }
 }
