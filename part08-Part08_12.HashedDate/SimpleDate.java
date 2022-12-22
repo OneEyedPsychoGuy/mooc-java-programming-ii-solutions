47314b06c3009a05e40fd60c6eed29b0e74f7bb5
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-21 20:51:08 -0800

    Finalize part08-Part08_12.HashedDate

---
+++ part08-Part08_12.HashedDate/SimpleDate.java
@@ -0,0 +1,69 @@
+public class SimpleDate {
+    private int day;
+    private int month;
+    private int year;
+
+    public SimpleDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public boolean earlier(SimpleDate date) {
+        if(this.year < date.year) {
+            return true;
+        }
+
+        if(this.year == date.year &&
+            this.month < date.month) {
+            return true;
+        }
+
+        return this.year == date.year && 
+            this.month == date.month &&
+            this.day < date.day;
+    }
+
+    public int differenceInYears(SimpleDate date) {
+        if (earlier(date)) {
+            return date.differenceInYears(this);
+        }
+
+        int removed = 0;
+
+        if(this.month < date.month) {
+            removed = 1;
+        } else if(this.month == date.month && this.day < date.day) {
+            removed = 1;
+        }
+
+        return this.year - date.year - removed;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.day * this.month * this.year;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+
+        if(!(obj instanceof SimpleDate)) {
+            return false;
+        }
+
+        SimpleDate date = (SimpleDate) obj;
+
+        return this.day == date.day &&
+            this.month == date.month &&
+            this.year == date.year;
+    }
+
+    @Override
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+}
