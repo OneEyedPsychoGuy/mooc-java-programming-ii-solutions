fb1b7fc88c58b5c029f49fb8e696c90768ef8242
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-22 23:03:01 -0800

    Finalize part09-Part09_11.Herds

--- part09-Part09_11.Herds/Herd.java
+++ part09-Part09_11.Herds/Herd.java
@@ -2,25 +2,27 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Herd implements Movable {
-    private List<Movable> movables;
+    private List<Movable> herd;
 
     public Herd() {
-        this.movables = new ArrayList<>();
+        this.herd = new ArrayList<>();
     }
 
     public void addToHerd(Movable movable) {
-        this.movables.add(movable);
+        this.herd.add(movable);
     }
 
     @Override
     public void move(int dx, int dy) {
-        this.movables.forEach((movable) -> movable.move(dx, dy));
+        for(Movable movable : this.herd) {
+            movable.move(dx, dy);
+        }
     }
 
     @Override
     public String toString() {
         String string = "";
-        for(Movable movable : this.movables) {
+        for(Movable movable : this.herd) {
             string += movable.toString() + "\n";
         }
         return string;
