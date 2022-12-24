0fe781c81265b38116965422dd4aaa4b62e1107d
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-24 00:08:07 -0800

    Finalize part12-Part12_06.Numbers

--- part12-Part12_06.Numbers/Program.java
+++ part12-Part12_06.Numbers/Program.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         System.out.println("How many random numbers should be printed?");
-        int quantity = Integer.valueOf(keyboard.nextLine());
+        int quantity = Integer.valueOf(scanner.nextLine());
+        scanner.close();
         
         while(quantity > 0) {
             System.out.println(random.nextInt(11));
             quantity--;
         }
-
-        keyboard.close();
     }
 }
