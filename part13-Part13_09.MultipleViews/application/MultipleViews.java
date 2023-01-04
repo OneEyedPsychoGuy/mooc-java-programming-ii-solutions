ec6fbe68471ad417573f001a9667bae6024858f1
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-03 21:57:43 -0800

    Finalize part13-Part13_09.MultipleViews

--- part13-Part13_09.MultipleViews/application/MultipleViews.java
+++ part13-Part13_09.MultipleViews/application/MultipleViews.java
@@ -8,62 +8,37 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 
 public class MultipleViews extends Application {
     public static void main(String[] args) {
-        launch(MultipleViews.class);
+        Application.launch(MultipleViews.class);
     }
 
     @Override
-    public void start(final Stage window) {
+    public void start(Stage window) {
         Label firstViewLabel = new Label("First view!");
         Button firstViewButton = new Button("To the second view!");
-
         BorderPane firstViewBorder = new BorderPane();
         firstViewBorder.setTop(firstViewLabel);
         firstViewBorder.setCenter(firstViewButton);
-
-        final Scene firstScene = new Scene(firstViewBorder);
+        Scene firstScene = new Scene(firstViewBorder);
 
         Label secondViewLabel = new Label("Second view!");
         Button secondViewButton = new Button("To the third view!");
-
         VBox secondViewVBox = new VBox();
         secondViewVBox.getChildren().addAll(secondViewButton, secondViewLabel);
-
-        final Scene secondScene = new Scene(secondViewVBox);
+        Scene secondScene = new Scene(secondViewVBox);
 
         Label thirdViewLabel = new Label("Third view!");
         Button thirdViewButton = new Button("To the first view!");
-
         GridPane thirdViewGrid = new GridPane();
         thirdViewGrid.add(thirdViewLabel, 0, 0);
         thirdViewGrid.add(thirdViewButton, 1, 1);
+        Scene thirdScene = new Scene(thirdViewGrid);
 
-        final Scene thirdScene = new Scene(thirdViewGrid);
-
-        firstViewButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent handle) {
-                window.setScene(secondScene);
-            }
-        });
-
-        secondViewButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent handle) {
-                window.setScene(thirdScene);
-            }
-        });
-
-        thirdViewButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent handle) {
-                window.setScene(firstScene);
-            }
-        });
+        firstViewButton.setOnAction(handle -> window.setScene(secondScene));
+        secondViewButton.setOnAction(handle -> window.setScene(thirdScene));
+        thirdViewButton.setOnAction(handle -> window.setScene(firstScene));
 
         window.setScene(firstScene);
         window.show();
