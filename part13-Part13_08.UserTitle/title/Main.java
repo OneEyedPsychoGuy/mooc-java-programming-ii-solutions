cbd736b67fb595e5d6d11e0a96b31281ec1f6596
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-03 21:45:57 -0800

    Finalize part13-Part13_08.UserTitle

--- part13-Part13_08.UserTitle/title/Main.java
+++ part13-Part13_08.UserTitle/title/Main.java
@@ -1,20 +1,16 @@
 package title;
 
 import java.util.Scanner;
-
 import javafx.application.Application;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Write a title: ");
-        String title = keyboard.nextLine();
+        String title = scanner.nextLine();
+        scanner.close();
 
-        Application.launch(UserTitle.class,
-            "--title=" + title);
-
-        keyboard.close();
+        Application.launch(UserTitle.class, "--title=" + title);
     }
 }
