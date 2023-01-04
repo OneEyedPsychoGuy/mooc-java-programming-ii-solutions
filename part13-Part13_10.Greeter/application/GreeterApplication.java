6df0d7a9a3020fb40f219eaf325d03476d689a4d
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-03 22:16:13 -0800

    Finalize part13-Part13_10.Greeter

--- part13-Part13_10.Greeter/application/GreeterApplication.java
+++ part13-Part13_10.Greeter/application/GreeterApplication.java
@@ -10,24 +10,22 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 
 public class GreeterApplication extends Application {
     public static void main(String[] args) {
-        launch(GreeterApplication.class);
+        Application.launch(GreeterApplication.class);
     }
 
     @Override
-    public void start(final Stage window) {
+    public void start(Stage window) {
         Label instructionLabel = new Label("Enter your name and start.");
-        final TextField nameEntryTextField = new TextField();
+        TextField nameField = new TextField();
         Button startButton = new Button("Start");
 
         GridPane nameEntryLayout = new GridPane();
 
         nameEntryLayout.add(instructionLabel, 0, 0);
-        nameEntryLayout.add(nameEntryTextField, 0, 1);
+        nameEntryLayout.add(nameField, 0, 1);
         nameEntryLayout.add(startButton, 0, 2);
 
         nameEntryLayout.setPrefSize(300, 180);
@@ -38,21 +36,18 @@ public class GreeterApplication extends Application {
 
         Scene nameEntryView = new Scene(nameEntryLayout);
 
-        final Label welcomeText = new Label("Welcome!");
+        Label welcomeText = new Label("Welcome!");
 
         StackPane welcomeLayout = new StackPane();
         welcomeLayout.setPrefSize(300, 180);
         welcomeLayout.getChildren().add(welcomeText);
         welcomeLayout.setAlignment(Pos.CENTER);
 
-        final Scene welcomeView = new Scene(welcomeLayout);
+        Scene welcomeView = new Scene(welcomeLayout);
 
-        startButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                welcomeText.setText("Welcome " + nameEntryTextField.getText() + "!");
-                window.setScene(welcomeView);
-            }
+        startButton.setOnAction(event -> {
+            welcomeText.setText("Welcome " + nameField.getText() + "!");
+            window.setScene(welcomeView);
         });
 
         window.setScene(nameEntryView);
