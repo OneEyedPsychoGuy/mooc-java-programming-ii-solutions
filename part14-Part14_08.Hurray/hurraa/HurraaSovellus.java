318e6724e7c718deb50a169449bddb92b80adaa1
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2023-01-06 14:21:56 -0800

    Finalize part14-Part14_08.Hurray

--- part14-Part14_08.Hurray/hurraa/HurraaSovellus.java
+++ part14-Part14_08.Hurray/hurraa/HurraaSovellus.java
@@ -1,8 +1,6 @@
 package hurraa;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.media.AudioClip;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,26 +9,19 @@ import javafx.stage.Stage;
 
 public class HurraaSovellus extends Application {
     public static void main(String[] args) {
-        launch(HurraaSovellus.class);
+        Application.launch(HurraaSovellus.class);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        AudioClip sound = new AudioClip("file:Applause-Yannick_Lemieux.wav");
         BorderPane pane = new BorderPane();
-
         Button button = new Button("Hurray!");
+
+        button.setOnAction(event -> sound.play());
         pane.setCenter(button);
 
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                new AudioClip("file:Applause-Yannick_Lemieux.wav").play();
-            }
-        });
-
-        Scene scene = new Scene(pane, 600, 400);
-
-        stage.setScene(scene);
+        stage.setScene(new Scene(pane, 600, 400));
         stage.show();
     }
 }
